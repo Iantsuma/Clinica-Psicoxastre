97273e61ffe50576c587cdff8f9523535fc61f51
Pedro Augusto Eickhoff <pedroaeickhoff@gmail.com>
2024-05-28 00:09:02 -0300

    Seeder

--- ClinicaXastre/laravel/database/seeders/UserSeeder.php
+++ ClinicaXastre/laravel/database/seeders/UserSeeder.php
@@ -16,5 +16,12 @@ class UserSeeder extends Seeder
             'password' => Hash::make('12345678'), 
             'role' => 2,
         ]);
+
+        User::create([
+            'nome' => 'Psicologa',
+            'email' => 'psicologa@psicologa.com',
+            'password' => Hash::make('12345678'), 
+            'role' => 3,
+        ]);
     }
 }
